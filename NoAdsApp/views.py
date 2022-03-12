dec6f078dfd624009715b5996e50deb74ee7d569
Shivraj <shivunofficial003@gmail.com>
2022-03-12 15:10:37 +0530

    main page & backend

---
+++ NoAdsApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'main.html')
+    # return HttpResponse("this is homepage")
+
+def login(request):
+    return HttpResponse("this is about page")
